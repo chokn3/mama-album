638f40ebd97f050d7838114b7113f11996c94ede
chokn3 <jakeguevarra80@gmail.com>
2026-05-06 09:43:51 +0800

    hehe

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useState, useRef } from "react";
 const photos = [
   {
     front: {
-      url: "https://drive.google.com/file/d/1rHVz4J_CSTl91QDFzi50HSmx8yBF1QrS/view?usp=sharing",
+      url: "https://drive.google.com/uc?export=view&id=1rHVz4J_CSTl91QDFzi50HSmx8yBF1QrS",
       caption: "Summer at the beach",
       date: "July 2023",
       tilt: "-rotate-2",
